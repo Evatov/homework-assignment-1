e9e075eca2425c9cf2fbb23084a7ca42dfc3cf5c
Akbar Evatov <evatovgithub@gmail.com>
2024-09-11 21:14:33 +0500

    Add files via upload
    These are the questions we did in today's lab...

---
+++ problem7.cpp
@@ -0,0 +1,12 @@
+//
+// Created by Akbar Evatov on 11/09/24.
+//
+#include <iostream>
+using namespace std;
+int main() {
+    float x=0,y=0;
+    cin>>x>>y;
+    float t=(3*(x*x+3)/((y/4)+3));
+    cout<<"The output is: "<<t<<endl;
+    return 0;
+}
---
+++ problem8.cpp
@@ -0,0 +1,13 @@
+//
+// Created by Akbar Evatov on 11/09/24.
+//
+#include <iostream>
+using namespace std;
+int main() {
+    float p=3.14;
+    float d=0;
+    cin>>d;
+    float area=(p*d*d);
+    cout<<"The surface area of the sphere is "<<area;
+    return 0;
+}
